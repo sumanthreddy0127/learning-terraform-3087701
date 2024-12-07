330be93c01aca4f6325f63be82c6cb8f3f09d367
Sumanth Reddy Dubbaka <77742940+sumanthreddy0127@users.noreply.github.com>
2024-12-07 11:27:08 -0500

    load balancer

--- main.tf
+++ main.tf
@@ -48,6 +48,44 @@ resource "aws_instance" "web" {
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
+
+  name    = "blog-alb"
+  load_balancer_type = "application"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+Security_groups = module.blog_sg.security_group_id
+
+  
+
+  target_groups = {
+    {
+      name_prefix      = "blog"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      targets = {
+              target_id   = aws_instance.blog.id
+              port = 80
+    }
+  }
+}
+]
+
+http_tcp_listeners = {
+  port = 80
+  protocol = "http"
+  target_group_index = 0
+ }
+}
+  tags = {
+    Environment = "Dev"
+
+  }
+}
+
 module "security-group" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
